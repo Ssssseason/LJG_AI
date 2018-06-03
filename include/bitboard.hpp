5b483ebd351e2dd3b1a7fd3e1ef2d919c6d5ba99
ssssseason <3150102305@zju.edu.cn>
2018-06-03 21:58:23 +0800

    Clean to test in server

--- include/bitboard.hpp
+++ include/bitboard.hpp
@@ -1,9 +1,3 @@
-/*
- * @Author: Yixin Ji
- * @Date: 2018-06-01 11:13:21
- * @Last Modified by:   Yixin Ji
- * @Last Modified time: 2018-06-01 11:13:21
- */
 #ifndef BITBOARD_HPP
 #define BITBOARD_HPP
 #include <cstdint>
