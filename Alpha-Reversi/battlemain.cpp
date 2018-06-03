12cad1443420caf0355e249b07ee43e7fe2639c8
Ssssseason <3150102305@zju.edu.cn>
2018-06-03 23:52:02 +0800

    Update battle code

--- Alpha-Reversi/battlemain.cpp
+++ Alpha-Reversi/battlemain.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 
-#define MACHINE_TIME 5
+#define MACHINE_TIME 4
 
 #include<vector>
 #include <ctime>
@@ -24,65 +24,131 @@ action p2_engine(Role player, Bitboard b, Timer t);
 
 action run(machine f, Role player, Bitboard board, Timer t);
 
-void mainServer(bool first, int ai_seconds)
+void mainServer(int ai_seconds)
 {
+	int num = 10, i;
+	bool first = true;
 
-	MatrixXd board;
-	reversiInit(board);
-	Bitboard::init();
-	Bitboard b(0x810000000, 0x1008000000);
+	i = num;
+	while(i--){
+		MatrixXd board;
+		reversiInit(board);
+		Bitboard::init();
+		Bitboard b(0x810000000, 0x1008000000);
 
-	bool turn(first);
+		bool turn(first);
 
-	while (!reversiIsTerminal(board))
-	{
-		if (turn)
+		while (!reversiIsTerminal(board))
 		{
-			UCTTree uctt(board, first, Position(-1, -1));
-			Position best_AI_pos;
-			std::printf("Now it is machine move\n");
-			if (uctt.UCTSearch(ai_seconds, best_AI_pos))
+			if (turn)
 			{
-				std::printf("machien choose to do ");
-				std::printf("AI (%d, %d)\n", best_AI_pos.x(), best_AI_pos.y());
-				reversiStepOnPos(board, best_AI_pos, first);
-				b.takeAction(first? BLACK:WHITE, encode_action(best_AI_pos.x(), best_AI_pos.y()));
-				b.printBoard();
-			//std::cout << '\n' << board << std::endl;
+				UCTTree uctt(board, first, Position(-1, -1));
+				Position best_AI_pos;
+				//std::printf("Now it is machine move\n");
+				if (uctt.UCTSearch(ai_seconds, best_AI_pos))
+				{
+					//std::printf("machien choose to do ");
+					//std::printf("AI (%d, %d)\n", best_AI_pos.x(), best_AI_pos.y());
+					reversiStepOnPos(board, best_AI_pos, first);
+					b.takeAction(first? BLACK:WHITE, encode_action(best_AI_pos.x(), best_AI_pos.y()));
+					//b.printBoard();
+				//std::cout << '\n' << board << std::endl;
+				}
+				else
+				{
+					//std::cout << "PASS!" << std::endl;
+				}
+				turn = !turn;
 			}
 			else
 			{
-				std::cout << "PASS!" << std::endl;
-			}
-			turn = !turn;
-		}
-		else
-		{
-			//std::cout << '\n' << board << std::endl;
-			//int x, y;
-			//// std::cin >> "your move:please enter x-pos and then y pos like \' 4 5 \'" >> std::endl;
-			//// std::cin >> "x-pos ranges from 0 to 7. So is y-pos" >> std::endl;
-			//std::printf("your move. Please enter your decision\n");
-			//std::cin >> x >> y;
-			//reversiStepOnPos(board, Position(x, y), !first);
-			//std::cout << '\n' << board << std::endl;
-			//turn = !turn;
+				//std::cout << '\n' << board << std::endl;
+				//int x, y;
+				//// std::cin >> "your move:please enter x-pos and then y pos like \' 4 5 \'" >> std::endl;
+				//// std::cin >> "x-pos ranges from 0 to 7. So is y-pos" >> std::endl;
+				//std::printf("your move. Please enter your decision\n");
+				//std::cin >> x >> y;
+				//reversiStepOnPos(board, Position(x, y), !first);
+				//std::cout << '\n' << board << std::endl;
+				//turn = !turn;
 
-			Timer t(ai_seconds);
-			//action act = run(p1_engine,first?WHITE:BLACK, b, t);
-			action act = run(p2_engine,first?WHITE:BLACK, b, t);
-			if (act) {
-				b.takeAction(first ? WHITE : BLACK, act);
-				b.printBoard();
-				printf("%lf\n", t.getTimeLeft());
-				reversiStepOnPos(board, Position(decode_action(act)), !first);
+				Timer t(ai_seconds);
+				//action act = run(p1_engine,first?WHITE:BLACK, b, t);
+				action act = run(p2_engine,first?WHITE:BLACK, b, t);
+				if (act) {
+					b.takeAction(first ? WHITE : BLACK, act);
+					b.printBoard();
+					//printf("%lf\n", t.getTimeLeft());
+					reversiStepOnPos(board, Position(decode_action(act)), !first);
+				}
+				turn = !turn;
 			}
-			turn = !turn;
 		}
+		int black, white;
+		reversiColorCount(board, black, white);
+		printf("black: %d, white: %d\n", black, white);
 	}
-	int black, white;
-	reversiColorCount(board, black, white);
-	printf("black: %d, white: %d\n", black, white);
+
+	i = num;
+	first = false;
+	while(i--){
+		MatrixXd board;
+		reversiInit(board);
+		Bitboard::init();
+		Bitboard b(0x810000000, 0x1008000000);
+
+		bool turn(first);
+
+		while (!reversiIsTerminal(board))
+		{
+			if (turn)
+			{
+				UCTTree uctt(board, first, Position(-1, -1));
+				Position best_AI_pos;
+				//std::printf("Now it is machine move\n");
+				if (uctt.UCTSearch(ai_seconds, best_AI_pos))
+				{
+					//std::printf("machien choose to do ");
+					//std::printf("AI (%d, %d)\n", best_AI_pos.x(), best_AI_pos.y());
+					reversiStepOnPos(board, best_AI_pos, first);
+					b.takeAction(first? BLACK:WHITE, encode_action(best_AI_pos.x(), best_AI_pos.y()));
+					//b.printBoard();
+				//std::cout << '\n' << board << std::endl;
+				}
+				else
+				{
+					//std::cout << "PASS!" << std::endl;
+				}
+				turn = !turn;
+			}
+			else
+			{
+				//std::cout << '\n' << board << std::endl;
+				//int x, y;
+				//// std::cin >> "your move:please enter x-pos and then y pos like \' 4 5 \'" >> std::endl;
+				//// std::cin >> "x-pos ranges from 0 to 7. So is y-pos" >> std::endl;
+				//std::printf("your move. Please enter your decision\n");
+				//std::cin >> x >> y;
+				//reversiStepOnPos(board, Position(x, y), !first);
+				//std::cout << '\n' << board << std::endl;
+				//turn = !turn;
+
+				Timer t(ai_seconds);
+				//action act = run(p1_engine,first?WHITE:BLACK, b, t);
+				action act = run(p2_engine,first?WHITE:BLACK, b, t);
+				if (act) {
+					b.takeAction(first ? WHITE : BLACK, act);
+					//b.printBoard();
+					//printf("%lf\n", t.getTimeLeft());
+					reversiStepOnPos(board, Position(decode_action(act)), !first);
+				}
+				turn = !turn;
+			}
+		}
+		int black, white;
+		reversiColorCount(board, black, white);
+		printf("black: %d, white: %d\n", black, white);
+	}
 
 }
 
@@ -90,7 +156,7 @@ void battle() {
 	bool machine_first = false;
 	std::printf("-1 -> machine\n1 -> you\n the chess board is a 8*8 matrix ranging from 0 to 7\n");
 	std::printf("for example,\'2 3\' means the 3rd row and 4th column\n");
-	mainServer(machine_first, MACHINE_TIME);
+	mainServer(MACHINE_TIME);
 
 }
 
